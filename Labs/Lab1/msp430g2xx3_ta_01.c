05f8d006a1b8a6423f8350d1780099571a1c82f5
Caleb Kemere <ckemere@gmail.com>
2018-01-13 21:11:28 -0600

    Updated example file with comments

--- Labs/Lab1/msp430g2xx3_ta_01.c
+++ Labs/Lab1/msp430g2xx3_ta_01.c
@@ -45,6 +45,21 @@
 //******************************************************************************
 //  MSP430G2xx3 Demo - Timer_A, Toggle P1.0, CCR0 Cont. Mode ISR, DCO SMCLK
 //
+//  Description: Toggle P1.0 using software and TA_0 ISR. Toggles every
+//  50000 SMCLK cycles. SMCLK provides clock source for TACLK.
+//  During the TA_0 ISR, P1.0 is toggled and 50000 clock cycles are added to
+//  CCR0. TA_0 ISR is triggered every 50000 cycles. CPU is normally off and
+//  used only during TA_ISR.
+//  ACLK = n/a, MCLK = SMCLK = TACLK = default DCO
+//
+//           MSP430G2xx3
+//         ---------------
+//     /|\|            XIN|-
+//      | |               |
+//      --|RST        XOUT|-
+//        |               |
+//        |           P1.0|-->LED
+//
 //  D. Dang
 //  Texas Instruments Inc.
 //  December 2010
@@ -55,13 +70,13 @@
 
 int main(void)
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // ???
-  P1DIR |= 0x01;                            // ???
-  CCTL0 = CCIE;                             // ???
-  CCR0 = 50000;                             // ???
-  TACTL = TASSEL_2 + MC_2;                  // ???
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P1DIR |= 0x01;                            // P1.0 output
+  CCTL0 = CCIE;                             // CCR0 interrupt enabled
+  CCR0 = 50000;
+  TACTL = TASSEL_2 + MC_2;                  // SMCLK, contmode
 
-  __bis_SR_register(LPM0_bits + GIE);       // ???
+  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
 }
 
 // Timer A0 interrupt service routine
@@ -74,6 +89,6 @@ void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer_A (void)
 #error Compiler not supported!
 #endif
 {
-  P1OUT ^= 0x01;                            // ???
-  CCR0 += 50000;                            // ???
+  P1OUT ^= 0x01;                            // Toggle P1.0
+  CCR0 += 50000;                            // Add Offset to CCR0
 }
