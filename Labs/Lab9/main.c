55ba1a0c21e1b140465340e14710c54a42f16174
Caleb Kemere <ckemere@gmail.com>
2020-04-15 23:21:22 -0500

    Updated Lab 9 code to fix bug

--- Labs/Lab9/main.c
+++ Labs/Lab9/main.c
@@ -28,64 +28,56 @@ void wdt_setup(void) {
 
 }
 
+
 uint8_t spi_array[] = {0x00, 0x00, 0x00, 0x00,
-               0xEF, 0x70, 0x00, 0x00, // blue
-               0xEF, 0x00, 0x70, 0x00, // green
-               0xEF, 0x00, 0x00, 0x70, // red
-               0xEF, 0x70, 0x00, 0x70, // dimmer magenta/yellow
+               0xEF, 0x70, 0x00, 0x00, // LED CLOSEST
+               0xEF, 0x00, 0x70, 0x00, // 
+               0xEF, 0x00, 0x00, 0x70, // 
+               0xEF, 0x70, 0x00, 0x70, // LED FURTHEST
                0x00, 0x00, 0x00, 0x00};
 
 
-uint8_t led_history[] = {0xEF, 0x00, 0x00, 0x00,
-                         0xEF, 0x00, 0x00, 0x00,
-                         0xEF, 0x00, 0x00, 0x00,
-                         0xEF, 0x00, 0x00, 0x00};
-                        // Note that size is a nice power of 2!!!!
-
-int rainbow_idx, history_idx;
-
 void update_colors(int direction) {
-    int i;
-    uint8_t *lptr;
+    int i, j;
+
+    static int rainbow_idx = 0;
 
     rainbow_idx = rainbow_idx + 3*direction; // shift one LED
 
     // Check over or underflow of ptr
-    if (rainbow_idx >= sizeof(rainbow_rbg)) // loop at the end if needed!
-        rainbow_idx = rainbow_idx - sizeof(rainbow_rbg);
+    if (rainbow_idx >= (int) sizeof(rainbow_rbg)) // loop at the end if needed!
+        rainbow_idx = rainbow_idx - (int) sizeof(rainbow_rbg);
     if (rainbow_idx < 0)
-        rainbow_idx = rainbow_idx + sizeof(rainbow_rbg);
+        rainbow_idx = rainbow_idx + (int) sizeof(rainbow_rbg);
 
-    // Update circular buffer
-    history_idx = (history_idx  - 4) & 0x000F; // go to next entry in circular buffer
-            // Move backwards so that history makes sense
-
+    // Copy history
+    for (i = 3; i > 0; i--) {
+        for (j = 0; j < 4; j++) {
+            spi_array[4 + i*4 + j] = spi_array[i*4 + j];
+        }
+    }
+    
     for (i = 0; i < 3; i++) {
-        led_history[history_idx + i + 1] = rainbow_rbg[rainbow_idx + i];
+        spi_array[i + 5] = rainbow_rbg[rainbow_idx + i];
         // copy from rainbow array to LED history array skipping the first intensity entry
     }
 
-    // Copy LED values from history circular buffer
-    lptr = spi_array + 4; // point LED pointer at the beginning of the LED array
-    for (i = 0; i < sizeof(led_history); i++) {
-        *lptr++ = led_history[(history_idx + i) & 0x000F]; // note circular indexing
-    }
 
 }
+
 
 void main() {
 
     // LED Code
-    int i;
+    int i, next_change, last_change;
 
     BCSCTL3 |= LFXT1S_2;
     SPI_setup();
     wdt_setup();
 
-
     while(1) {
 
-        update_colors(3); // Example - increase temperature in steps of 3
+        update_colors(-3); // Example - increase temperature in steps of 3
 
         for (i = 0; i <= sizeof(spi_array); i++) { // Send Color Frame
           while(!(IFG2 & UCA0TXIFG)); // Buffer ready?
