fba6b8643c20e3e4db58693406863c70768bae87
Caleb Kemere <ckemere@gmail.com>
2022-01-07 17:50:26 -0600

    Updated panelizing script - new eagle version

--- PanelizingScripts/RecompileBoard.sh
+++ PanelizingScripts/RecompileBoard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=$PATH:/home/ckemere/eagle-9.2.2/
+PATH=$PATH:/home/ckemere/Code/eagle-9.6.2/
 
 for i in "$@"; do
   prefix=$(echo $i | sed -e "s/\(.*\).brd/\1/");
@@ -7,7 +7,7 @@ for i in "$@"; do
 
   echo $student;
 
-  eagle -X -dCAMJOB -j/mnt/d/Development/Design/ELEC327/PCBs/AdvancedCircuitsBarebones.cam -o. "$prefix.brd"
+  eagle -X -dCAMJOB -j/home/ckemere/Code/elec327/PCBs/AdvancedCircuitsBarebones.cam -o. "$prefix.brd"
   # eagle -X -d GERBER_RS274X -o "$prefix.GTL" "$prefix.brd" Top Pads Vias Dimension > /dev/null
   # eagle -X -d GERBER_RS274X -o "$prefix.GBL" "$prefix.brd" Bottom Pads Vias > /dev/null
   # eagle -X -d GERBER_RS274X -o "$prefix.GML" "$prefix.brd" Dimension Milling > /dev/null
