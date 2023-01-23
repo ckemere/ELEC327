3319baf14d88e95f8c1d50afdd42c8f48c5dabf7
Caleb Kemere <ckemere@gmail.com>
2023-01-23 15:53:01 -0600

    Restored scarlett's pushes

--- Labs/Lab1/lab1_skeleton.c
+++ Labs/Lab1/lab1_skeleton.c
@@ -1,37 +1,38 @@
-#include <msp430.h> 
+//***************************************************************************************
+//  MSP430 Blink the LED Demo - Software Toggle P1.0
+//
+//  Description; Toggle P1.0 by xor'ing P1.0 inside of a software loop.
+//  ACLK = n/a, MCLK = SMCLK = default DCO
+//
+//                MSP430x5xx
+//             -----------------
+//         /|\|              XIN|-
+//          | |                 |
+//          --|RST          XOUT|-
+//            |                 |
+//            |             P1.0|-->LED
+//
+//  Texas Instruments, Inc
+//  July 2013
+//***************************************************************************************
 
+#include <msp430.h>
 
-
-int main(void)
-{
+void main(void) {
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings  P1DIR |= 0x01;                            // P1.0 output
-    TA0CCTL0 = CCIE;                        // TA0CCR0 interrupt enabled
-    TA0CCR0 = 0x2000;
-    TA0CTL = TASSEL_2 + MC_1 + ID_3; // SMCLK, up-mode, Divide clock by 8
-
+                                            // to activate previously configured port settings
     P1DIR |= 0x01;                          // Set P1.0 to output direction
 
-    __bis_SR_register(GIE);       // Enable interrupt
-    while (1) {
-        __bis_SR_register(LPM0_bits);       // Enter LPM0
-        TA0CCR0 += 0x2000;
-        P1OUT ^= 0x01;                            // Toggle P1.0
+    for(;;) {
+        volatile unsigned int i;            // volatile to prevent optimization
+
+
+        P1OUT ^= 0x01;                      // Toggle P1.0 using exclusive-OR
+
+        i = 10000;                          // SW Delay
+        do i--;
+        while(i != 0);
     }
-
 }
 
-// Timer A0 interrupt service routine
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A (void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer_A (void)
-#else
-#error Compiler not supported!
-#endif
-{
-  __bic_SR_register_on_exit(LPM0_bits);
-}
