4559f05048eca43683077b8fa33087b25e7b2f86
Caleb Kemere <ckemere@gmail.com>
2025-01-26 20:31:15 -0600

    Clocked version of code

---
+++ Code/ClockedPulseWidth/README.md
@@ -0,0 +1,48 @@
+## Summary
+
+This program uses the timer module capture and compare functionality to detect the duration of pulses
+on an input pin (Pin PB20). It uses the TIMG12 timer module, which has a 32-bit counter register, to enable high
+resolution pulse width measurements from the range of microseconds to seconds. When pulses are detected,
+the duration of the pulse (in 32 MHz clock cycles from rising edge to falling edge) is reported over 
+the UART connection as a string ("=     XXXXX\r\n"), and the red Launchpad LED is briefly illuminated. 
+A second timer module is used to trigger a heartbeat UART message every second if no pulse is detected. In 
+addition to the heartbeat, the PA15 pin is briefly pulsed. To verify functionality, the PA17 and PB20 pins
+can be jumpered.
+
+A python script, `read_data.py` is included that opens the serial port and records the values being
+transmitted to a file.  
+
+
+## Peripherals & Pin Assignments
+
+| Peripheral | Pin | Function |
+| --- | --- | --- |
+| GPIOA | PA15 | Standard Output |
+| GPIOA | PB20 | Input to Timer Capture |
+| SYSCTL |  |  |
+| TIMG0 |  | Periodic interrupt wakes system to send heartbeat |
+| TIMG12 |  | Capture on rising edge and falling edge |
+| UART0 | PA11 | RX Pin |
+| UART0 | PA10 | TX Pin |
+| EVENT |  |  |
+| DMA |  | Used for serial transmission |
+| DEBUGSS | PA20 | Debug Clock |
+| DEBUGSS | PA19 | Debug Data In Out |
+
+## Example Usage
+In Code Composer Studio, using the `Run` menu, `Load`, you can load the preocompiled firmware
+found in the file [pulsewidth_to_uart.out](pulsewidth_to_uart.out) onto your Launchpad. 
+
+When plugged into the computer, the Launchpad creates two serial ports. In our brief experimentation,
+on Windows, the serial data appears to typically appear on `COM4` (Device Manager will show two new
+`COM` ports when the Launchpad is connected. It will be one of them!).  On MacOS, it appears on something like
+`/dev/tty.usbmodemMG3500011` (run `ls /dev/tty*` in a Terminal window to see possible choices.)
+
+Connect to a serial terminal (i.e., PuTTY on Windows) to view the UART output or use the `read_data.py` 
+python script to log data.
+
+Note that the serial configuraiton should be:
+- 115200 baud rate
+- one stop bit
+- no parity
+
---
+++ Code/ClockedPulseWidth/clocked_pulsewidth_to_uart.c
@@ -0,0 +1,215 @@
+/*
+ * Copyright (c) 2021, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <ti/devices/msp/msp.h>
+#include <ti/driverlib/driverlib.h>
+#include <ti/driverlib/m0p/dl_core.h>
+
+#include "ti_msp_dl_config.h"
+
+
+volatile uint32_t externalClk = 0;
+volatile uint32_t ioValue = 0;
+
+#define false 0
+#define true 1
+
+char gWelcomeMsg[] = "\r\n==== MSPM0 Console Test ====\r\n";
+char gNoPulseMsg[] = "No pulse detected in last second. Generating pulse.\r\n";
+char gTimingMsg[60];
+
+volatile int gConsoleTxTransmitted, gConsoleTxDMATransmitted, timerExpired;
+
+
+#define TIMER_CAPTURE_DURATION (CAPTURE_0_INST_LOAD_VALUE)
+volatile int pulseCaptureDetected;
+
+
+void UART_Console_write(const char *data, uint16_t size)
+{
+    DL_DMA_setSrcAddr(DMA, DMA_CH0_CHAN_ID, (uint32_t)(data));
+    DL_DMA_setDestAddr(DMA, DMA_CH0_CHAN_ID, (uint32_t)(&UART_0_INST->TXDATA));
+    DL_DMA_setTransferSize(DMA, DMA_CH0_CHAN_ID, size);
+
+    DL_SYSCTL_disableSleepOnExit();
+
+    DL_DMA_enableChannel(DMA, DMA_CH0_CHAN_ID);
+
+    while (false == gConsoleTxDMATransmitted) {
+        __WFE();
+    }
+
+    while (false == gConsoleTxTransmitted) {
+        __WFE();
+    }
+
+    gConsoleTxTransmitted    = false;
+    gConsoleTxDMATransmitted = false;
+}
+
+__attribute__((always_inline))
+void blink_led(void) {
+    GPIO_LEDS_PORT->DOUTSET31_0 = GPIO_LEDS_USER_TEST_PIN;
+    __NOP();
+    __NOP();
+    __NOP();
+    __NOP();
+
+    GPIO_LEDS_PORT->DOUTCLR31_0 = GPIO_LEDS_USER_TEST_PIN;
+}
+
+volatile uint32_t tRise, tFall, previousTFall = 0;
+
+
+int main(void)
+{
+    timerExpired           = false;
+    gConsoleTxTransmitted    = false;
+    gConsoleTxDMATransmitted = false;
+
+    int pulseCapturedRecently = false;
+
+    uint32_t pulseWidth = 0;
+    uint32_t spaceWidth = 0;
+
+    SYSCFG_DL_init();
+
+
+    NVIC_EnableIRQ(UART_0_INST_INT_IRQN);
+    NVIC_EnableIRQ(TIMER_0_INST_INT_IRQN);
+    NVIC_EnableIRQ(GPIO_CLK_GRP_INT_IRQN);
+
+    pulseCaptureDetected = false;
+
+    /* Write welcome message */
+    UART_Console_write(&gWelcomeMsg[0], sizeof(gWelcomeMsg));
+
+    while (1) {
+        if (timerExpired == true) {
+            timerExpired  = false;
+            if (false == pulseCapturedRecently) {
+                UART_Console_write(&gNoPulseMsg[0], sizeof(gNoPulseMsg));
+                blink_led();
+            }
+            else {
+                pulseCapturedRecently = false;
+            }
+            blink_led();
+        }
+        else if (true == pulseCaptureDetected) {
+            pulseCaptureDetected = false;
+            pulseCapturedRecently = true;
+
+            // times latched in ISR
+            pulseWidth = tFall - tRise;
+            spaceWidth = tRise - previousTFall;
+
+
+            GPIO_LEDS_PORT->DOUTSET31_0 = GPIO_LEDS_USER_LED_1_PIN;
+            delay_cycles(100);
+            GPIO_LEDS_PORT->DOUTCLR31_0 = GPIO_LEDS_USER_LED_1_PIN;
+
+//            __BKPT(0);
+            sprintf(gTimingMsg, "=%10u,%10u\r\n", spaceWidth, pulseWidth);
+            UART_Console_write(&gTimingMsg[0], 25);
+        }
+        else {
+            __WFI(); // __WFE();?
+        }
+
+    }
+}
+
+void UART_0_INST_IRQHandler(void)
+{
+    switch (DL_UART_Main_getPendingInterrupt(UART_0_INST)) {
+        case DL_UART_MAIN_IIDX_EOT_DONE:
+            gConsoleTxTransmitted = true;
+            break;
+        case DL_UART_MAIN_IIDX_DMA_DONE_TX:
+            gConsoleTxDMATransmitted = true;
+            break;
+        default:
+            break;
+    }
+}
+
+void TIMER_0_INST_IRQHandler(void)
+{
+    switch (DL_Timer_getPendingInterrupt(TIMER_0_INST)) {
+        case DL_TIMER_IIDX_ZERO:
+            timerExpired = true;
+            break;
+        default:
+            break;
+    }
+}
+
+
+//    __STATIC_INLINE uint32_t DL_GPIO_getEnabledInterruptStatus(
+//        GPIO_Regs* gpio, uint32_t pins)
+//    {
+//        return (gpio->CPU_INT.MIS & pins);
+//    }
+
+
+void GROUP1_IRQHandler(void)
+{
+//    uint32_t gpioA = DL_GPIO_getEnabledInterruptStatus(GPIOA,
+//        GPIO_SWITCHES_USER_SWITCH_1_PIN | GPIO_SWITCHES_USER_SWITCH_3_PIN);
+
+    externalClk++;
+
+    uint32_t newIOValue = DL_GPIO_readPins(GPIO_CLK_GRP_PORT, GPIO_CLK_GRP_GPIO_PULSES_IN_PIN);
+
+    if (newIOValue == ioValue) { // unchanged. do nothing
+//        SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;
+
+    }
+    else if (ioValue) { // old value was true (1-ish), now we're false, so falling edge
+        ioValue = newIOValue;
+        previousTFall = tFall;
+        tFall = externalClk;
+        pulseCaptureDetected = true;
+        SCB->SCR &= ~(SCB_SCR_SLEEPONEXIT_Msk); // disable sleep on exit
+        // wake up from sleep
+    }
+    else {
+        ioValue = newIOValue;
+        tRise = externalClk;
+//        SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;
+    }
+
+}
+
